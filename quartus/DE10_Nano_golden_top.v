86834ad9aa7147195e9bee76f62af4e7eb2c9604
sizeru <gaafts@gmail.com>
2024-03-29 13:11:38 -0500

    Document how we should run the testbenches

--- quartus/DE10_Nano_golden_top.v
+++ quartus/DE10_Nano_golden_top.v
@@ -140,14 +140,99 @@ module DE10_Nano_golden_top(
 //  Structural coding
 //=======================================================
 
-wire [7:0] KEY_N;
-assign KEY_N = KEY;
+`define RS_SIZE 2
+`define ROB_SIZE 2
+`define REG_SIZE 64
+`define GPR_COUNT 32
+`define GPR_IDX_SIZE $clog2(`GPR_COUNT)
 
-assign LED[0] = ~KEY_N[0];
-assign LED[1] = ~KEY_N[1];
-assign LED[3] = ~KEY_N[1] | ~KEY_N[0];
-assign LED[4] = ~KEY_N[1] & ~KEY_N[0];
-assign LED[5] = ~KEY_N[1] ^ ~KEY_N[0];
+// NOTE(Nate): I stopped here. Contemplating a couple things.
+// - Change rs_entry to op_entry since it only deals with one op. Then have
+//   rs_entry have two operands in it.
+// - Change the re-order-buffer to use a struct
+typedef struct packed {
+    logic valid;
+    logic [`GPR_IDX_SIZE-1:0] gpr_idx;
+    logic [`REG_SIZE-1:0] value;
+} rs_op;
 
+typedef struct packed {
+    rs_op op1;
+    rs_op op2;
+} rs_entry;
 
+typedef struct packed {
+    rs_entry [`RS_SIZE-1:0] rs;
+    logic [`ROB_SIZE-1:0] rob;
+} debug_info;
+
+// Upon a reset signal, the computer should attempt to perform a read from
+// a set place in memory. We need to decide where it should read from. On x86,
+// this is 0xfffffff0. This is the memory address of the BIOS. The BIOS
+// contains the ability to load and run the bootloader. In our machine, the
+// BIOS needs to be the code which prepares to receive input from the test
+// bench. In other words, the steps are as follows:
+// 1. Upon receiving a reset signal, the processor begins executing from
+// address 0xfffffff0. This is where our ROM program is. The job of this 
+// program is to wait for input (via a button), before it begins running 
+// our testbench.
+// 2. We load in the program into memory. The starting memory address must
+// always be at a particular location in memory.
+// 3. We run the program. Upon the final return, it should return to the loop
+// in the BIOS.
+//
+// So in essence, we are creating a small operating system for our program.
+module core #(
+    parameter int RS_SIZE = 2,
+    parameter int ROB_SIZE = 2
+) (
+    input logic i_clk,
+    input logic i_reset,
+		input logic [31:0] i_instruction,
+		output logic o_commit,
+    output debug_info o_debug
+);
+    always_ff @(posedge i_clk) begin : main
+        if (i_reset) begin
+            o_debug.rs[0] <= {1'b0, {`GPR_IDX_SIZE{1'b0}}, `REG_SIZE'b0};
+            // NOTE(Nate): Need to use replication for expressions
+            o_debug.rob <= 0;
+        end else begin
+            o_debug.rs[0] <= o_debug.rs[0].op1 + 1;
+        end
+    end : main
+endmodule : core
+
+module testbench ();
+    localparam int RESERVATION_STATION_SIZE = 2;
+    localparam int REORDER_BUFFER_SIZE = 2;
+
+    debug_info debug_data;
+    logic clk;
+    logic reset;
+
+    core #(.RS_SIZE(2), .ROB_SIZE(2)) cpu_core (.o_debug(debug_data), .i_clk(clk), .i_reset(reset));
+
+    initial begin 
+        clk = 0;
+        reset = 1;
+        #5;
+        clk = 1; 
+        #5;
+        clk = 0;
+        reset = 0;
+        for (int i = 0; i < 8; i++) begin
+            #5;
+            clk = ~clk;
+            $display("\nclk: %b Debug: %x", clk, debug_data.rs[0]);
+            $write("Reservation Station: op1_valid: %1h | op1_value: %d | op1_gpr_idx: %d | op2_valid: %d | op2_value: %d | op2_gpr_idx: %d",
+                debug_data.rs[0].op1.valid,
+                debug_data.rs[0].op1.value,
+                debug_data.rs[0].op1.gpr_idx,
+                debug_data.rs[0].op2.valid,
+                debug_data.rs[0].op2.value,
+                debug_data.rs[0].op2.gpr_idx
+            );
+        end
+    end
 endmodule
